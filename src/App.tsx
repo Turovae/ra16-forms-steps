d59d77b7fc38d2c0f3f9b1fa1f30c10cca361dbb
Turovae <ekaterina.turova@mail.ru>
2023-05-22 17:26:01 +0700

    form structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className='steps-form'>
+        <div className="form-col">
+          <label className='form-group'>
+            <span className='form-hint'>Дата (ДД.ММ.ГГГГ)</span>
+            <input type="date" className='form-control' name='date'/>
+          </label>
+        </div>
+        <div className="form-col">
+          <label className='form-group'>
+            <span className='form-hint'>Пройдено км</span>
+            <input type="text" className='form-control' name='distance'/>
+          </label>
+        </div>
+        <div className="form-col">
+          <button type="submit" className='form-control'>OK</button>
+        </div>
+      </form>
     </>
   )
 }
